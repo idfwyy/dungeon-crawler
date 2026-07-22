2834eecb350c938e3601e2a90094f3eb0083bf85
Romie <249513315+idfwyy@users.noreply.github.com>
2026-07-21 21:58:15 -0500

    Add attack_power and attack method for combat between characters

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 class Player:
-    def __init__(self, name, health):
+    def __init__(self, name, health, attack_power):
         self.name = name
         self.health = health
+        self.attack_power = attack_power
 
     def take_damage(self, amount):
         self.health = self.health - amount
-        return self.health 
+        return self.health
     
-hero = Player("AARON", 100)
+    def attack(self,target):
+        target.take_damage(self.attack_power)
+
+class Enemy:
+    def __init__(self,name,health, attack_power):
+        self.name = name
+        self.health = health
+        self.attack_power = attack_power
+
+    def take_damage(self,amount):
+        self.health = self.health - amount
+        return self.health
+    
+    def attack(self,target):
+        target.take_damage(self.attack_power)
+    
+    
+
+hero = Player("AARON", 100, 32)
 hero.take_damage(40)
 print(hero.health)
+
+goblin = Enemy("Goblin", 100,65)
+goblin.take_damage(20)
+print(goblin.health)
+
+hero.attack(goblin)
+print(goblin.health)
